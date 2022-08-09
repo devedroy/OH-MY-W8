28cc53cf7cef4a1b8970765362ef1eaf36aae17f
Roy <devedroy.dr@gmail.com>
2022-08-09 13:58:04 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/example/ohmyw8/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.example.ohmyw8;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, SelectorActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+}
