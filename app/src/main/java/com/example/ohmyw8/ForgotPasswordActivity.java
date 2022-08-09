f46eb429fd32a4762d133367135c630a52fe49a1
Roy <devedroy.dr@gmail.com>
2022-08-09 18:25:48 +0530

    Sign In Intents

---
+++ app/src/main/java/com/example/ohmyw8/ForgotPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.example.ohmyw8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
