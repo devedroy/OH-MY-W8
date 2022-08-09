b5047e1848049aba0c182062064dc07def8df4a9
Roy <devedroy.dr@gmail.com>
2022-08-09 14:45:07 +0530

    SelectorActivity Intents Added

---
+++ app/src/main/java/com/example/ohmyw8/LocationSelectorActivity.java
@@ -0,0 +1,14 @@
+package com.example.ohmyw8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationSelectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_selector);
+    }
+}
