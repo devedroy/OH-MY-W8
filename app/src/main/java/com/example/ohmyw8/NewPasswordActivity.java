7df628e1f3558320808a217c54c8e816d5418c1d
Roy <devedroy.dr@gmail.com>
2022-08-10 17:56:16 +0530

    new pass new

--- app/src/main/java/com/example/ohmyw8/NewPasswordActivity.java
+++ app/src/main/java/com/example/ohmyw8/NewPasswordActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewPasswordActivity extends AppCompatActivity {
-
+//new pass
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
