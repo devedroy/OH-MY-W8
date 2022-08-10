b9ba051308ab34a33ce9b00b09117a0a41baa3e8
Roy <devedroy.dr@gmail.com>
2022-08-10 16:51:45 +0530

    SignUpActivity designed

---
+++ app/src/main/java/com/example/ohmyw8/MNVActivity.java
@@ -0,0 +1,14 @@
+package com.example.ohmyw8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MNVActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mnvactivity);
+    }
+}
--- app/src/main/java/com/example/ohmyw8/SignUpActivity.java
+++ app/src/main/java/com/example/ohmyw8/SignUpActivity.java
@@ -1,8 +1,10 @@
 package com.example.ohmyw8;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void backSignUp(View view) {
+        onBackPressed();
+    }
+
+    public void login(View view) {
+        startActivity(new Intent(this, SignInActivity.class));
+    }
+
+    public void signUpAccount(View view) {
+        startActivity(new Intent(this, MNVActivity.class));
     }
 }
