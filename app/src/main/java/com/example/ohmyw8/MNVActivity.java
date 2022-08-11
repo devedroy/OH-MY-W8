674c43265cbc15a2fcce6569d719b19e8e76b454
Roy Devpreyo <devedroy.dr@gmail.com>
2022-08-11 17:00:49 +0530

    MNV designed

--- app/src/main/java/com/example/ohmyw8/MNVActivity.java
+++ app/src/main/java/com/example/ohmyw8/MNVActivity.java
@@ -1,5 +1,6 @@
 package com.example.ohmyw8;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,5 +16,16 @@ public class MNVActivity extends AppCompatActivity {
 
     public void backMNV(View view) {
         onBackPressed();
+    }
+
+    public void resendOTPMnv(View view) {
+    }
+
+    public void submitMNV(View view) {
+        startActivity(new Intent(this, HomePageActivity.class));
+    }
+
+    public void editPhoneNumber(View view) {
+        startActivity(new Intent(this, SignUpActivity.class));
     }
 }
