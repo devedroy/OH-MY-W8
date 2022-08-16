ab32bc859d7d468f234c3c50a2283960975a4ab3
Roy Devpreyo <devedroy.dr@gmail.com>
2022-08-16 14:27:03 +0530

    SelectLocationActivity UI designed

--- app/src/main/java/com/example/ohmyw8/HomePageActivity.java
+++ app/src/main/java/com/example/ohmyw8/HomePageActivity.java
@@ -1,15 +1,27 @@
 package com.example.ohmyw8;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class HomePageActivity extends AppCompatActivity {
+
+    ViewPager2 vpBanners;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        // res
+
+        initViews();
+    }
+
+    private void initViews() {
+        vpBanners = findViewById(R.id.vpBanners);
+    }
+
+    public void locationSelector(View view) {
     }
 }
---
+++ app/src/main/java/com/example/ohmyw8/SelectLocationActivity.java
@@ -0,0 +1,18 @@
+package com.example.ohmyw8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SelectLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_location);
+    }
+
+    public void backLocationSelect(View view) {
+    }
+}
